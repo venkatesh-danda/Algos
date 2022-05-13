29302c93cf80424bc30b85d1abb158833c3d8c7c
radanda <ravi.sankar.danda@oracle.com>
2022-05-13 19:48:30 +0530

    Adding ArrayBasics

---
+++ src/arrays/ArraysBasics.java
@@ -0,0 +1,11 @@
+package arrays;
+
+public class ArraysBasics {
+	public static void main(String args[]) {
+		int arr[];
+		arr = new int[5];
+		arr[0] = 1;
+		
+		System.out.println(arr[4]);
+	}
+}
