0ad02a90e39c34638bde67d6d20718ff77c1938d
radanda <ravi.sankar.danda@oracle.com>
2022-07-03 18:45:18 +0530

    modified BinaryTree

---
+++ src/trees/DemoBinaryTree.java
@@ -0,0 +1,17 @@
+package trees;
+
+public class DemoBinaryTree {
+	
+	public static void main(String args[]) {
+		BinaryTree tree = new BinaryTree();
+		Node root = new Node(1);
+		root.left = new Node(2);
+		root.left.right = new Node(4);
+		root.left.left = new Node(3);
+		root.left.right.left = new Node(5);
+		root.left.right.right = new Node(6);
+		root.left.right.right.right = new Node(7);
+		
+		tree.leftView(root);
+	}
+}
